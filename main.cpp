67595c81e59ee657bc86e29654ea69f4b0ccb4d6
Mattia222 <127505321+Mattia222@users.noreply.github.com>
2023-05-04 19:58:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
+   int primo=1, secondo=1, terzo=0, n=0;
+
+    cin >> n;
+
+    cout << 1 << endl;
+    cout << 1 << endl;
+    for (int i = 0; i >= 0; i++) {
+        terzo = primo + secondo;
+        if (terzo>n) {
+            break;
+        }
+        cout << terzo << endl;
+        secondo = primo;
+        primo = terzo;
+        }
    return 0;
 }
